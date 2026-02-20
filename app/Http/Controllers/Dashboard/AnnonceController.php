7d5752f4057c924fecb9ec2fc90edee527a25edb
Samuel-Romaric <samuelromaric2015@gmail.com>
2026-02-20 12:22:42 +0000

    ns

--- app/Http/Controllers/Dashboard/AnnonceController.php
+++ app/Http/Controllers/Dashboard/AnnonceController.php
@@ -33,7 +33,7 @@ class AnnonceController extends Controller
         if ($request->hasFile('fichier_pdf')) {
             $file = $request->file('fichier_pdf');
             $filename = time() . '_' . $file->getClientOriginalName();
-            $filePath = $file->storeAs('/storage', $filename);
+            $filePath = $file->storeAs('/storage/annonces/', $filename);
         }
 
         // Enregistrement de l'annonce dans la base de données
